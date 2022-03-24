e00d3751e5efef9f94f2f1ba670ae0576c3b7d25
saidoud <develop.softoud@gmail.com>
2022-03-24 21:38:15 +0100

    ProductSkeloton

--- src/components/ui-components/ProductSkeloton.jsx
+++ src/components/ui-components/ProductSkeloton.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Grid, Skeleton, Stack } from '@mui/material';
 
 function ProductSkeloton() {
-    return <div>ProductSkeloton</div>;
+    return (
+        <Grid container spacing={4}>
+            {Array.from(new Array(6)).map((_, index) => (
+                <Grid item xs={12} sm={6} md={4} key={index}>
+                    <Stack spacing={1}>
+                        <Skeleton variant="rectangular" height={260} />
+                        <Skeleton variant="text" width={'50%'} />
+                    </Stack>
+                </Grid>
+            ))}
+        </Grid>
+    );
 }
 
 export default ProductSkeloton;
--- src/views/ProductList.jsx
+++ src/views/ProductList.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid } from '@mui/material';
 import FilterCatgory from 'components/FilterCatgory';
+import ProductSkeloton from 'components/ui-components/ProductSkeloton';
 
 function ProductList() {
+    const [loading, setLoading] = useState(true);
     return (
-        <Grid container spacing={2}>
+        <Grid container spacing={4}>
             {/* Category Filter */}
             <Grid item md={3} sx={{ display: { xs: 'none', md: 'block' } }}>
                 <FilterCatgory />
             </Grid>
             {/* Product List */}
-            <Grid item>
-                <Grid item xs={12} md={9}>
+            <Grid item xs={12} md={9}>
+                {loading ? (
+                    <ProductSkeloton />
+                ) : (
                     <Grid container spacing={2}>
                         {/* {products.map((item, index) => (
                             <Grid item xs={12} sm={6} md={4} key={index}>
@@ -19,7 +23,7 @@ function ProductList() {
                             </Grid>
                         ))} */}
                     </Grid>
-                </Grid>
+                )}
             </Grid>
         </Grid>
     );
